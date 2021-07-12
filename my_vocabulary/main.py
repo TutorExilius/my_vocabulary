414e6c2702d53409bf157b7ca276979ce636968a
Tutor Exilius <tutorexilius@gmail.com>
2021-07-12 20:34:33 +0200

    refactor: formatted code / blacked

--- my_vocabulary/main.py
+++ my_vocabulary/main.py
@@ -1,6 +1,7 @@
 import configparser
 
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
 from models.general import Vocabulary
 
@@ -8,7 +9,6 @@ alembic_config = configparser.ConfigParser()
 alembic_config.read("alembic.ini")
 database_url = alembic_config["alembic"]["sqlalchemy.url"]
 
-from sqlalchemy import create_engine
 engine = create_engine(database_url)
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -19,5 +19,6 @@ def main():
     session.add(voc_1)
     session.commit()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- my_vocabulary/models/general.py
+++ my_vocabulary/models/general.py
@@ -8,17 +8,16 @@ from sqlalchemy.types import Enum
 Base = declarative_base()
 metadata = Base.metadata
 
+
 class Language(enum.Enum):
     english = 0
     french = 1
     german = 2
 
+
 class Vocabulary(Base):
-    __tablename__ = 'vocabulary'
+    __tablename__ = "vocabulary"
 
     id = Column(INTEGER(11), primary_key=True)
     word = Column(String)
     language = Column(Enum(Language), default=Language.english)
-
-
-
