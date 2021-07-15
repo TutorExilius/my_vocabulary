790f6bb819ae78ed8fbb5b9214f10c16ed4cc5ae
Tutor Exilius <tutorexilius@gmail.com>
2021-07-15 22:22:58 +0200

    feat: adapt models, fixed read path of settings.ini, clean up db migrations

--- my_vocabulary/main.py
+++ my_vocabulary/main.py
@@ -1,23 +1,29 @@
 import configparser
+from datetime import datetime as dt
 
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-from models.general import Language, Vocabulary
+from models.general import Language, Entry, Note
 
 alembic_config = configparser.ConfigParser()
-alembic_config.read("alembic.ini")
-database_url = alembic_config["alembic"]["sqlalchemy.url"]
+alembic_config.read("../alembic.ini")
+database_url = alembic_config["alembic"]["sqlalchemy.url"].replace(
+    "sqlite:///", "sqlite:///../"
+)
 
 engine = create_engine(database_url)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+import logging
+
+logging.basicConfig()
+logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
+
 
 def main():
-    voc_1 = Vocabulary(word="test", language=Language.english)
-    session.add(voc_1)
-    session.commit()
+    pass
 
 
 if __name__ == "__main__":
--- my_vocabulary/models/general.py
+++ my_vocabulary/models/general.py
@@ -1,9 +1,10 @@
 import enum
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String
-from sqlalchemy.dialects.mysql import INTEGER
-from sqlalchemy.types import Enum
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.types import Boolean, DateTime, Enum, Integer, String
+
 
 Base = declarative_base()
 metadata = Base.metadata
@@ -15,9 +16,25 @@ class Language(enum.Enum):
     german = 2
 
 
-class Vocabulary(Base):
-    __tablename__ = "vocabulary"
+class Entry(Base):
+    __tablename__ = "entry"
 
-    id = Column(INTEGER(11), primary_key=True)
-    word = Column(String)
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, nullable=False)
+
+    text = Column(String, nullable=False)
     language = Column(Enum(Language), nullable=False)
+    audio_file = Column(String)
+    image_file = Column(String)
+    notes = relationship("Note", back_populates="entry")
+
+
+class Note(Base):
+    __tablename__ = "note"
+
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, nullable=False)
+
+    text = Column(String, nullable=False)
+    entry_id = Column(Integer, ForeignKey("entry.id"))
+    entry = relationship("Entry", back_populates="notes", lazy="joined")
